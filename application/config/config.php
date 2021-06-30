174ed7a58de38d258c5a68c8e10ce58f640b2b0f
hoshigakikisame <rianatgithub@gmail.com>
2021-06-30 22:12:17 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://timortravell.000webhostapp.com/';
+$config['base_url'] = 'https://ayo-dolan.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
